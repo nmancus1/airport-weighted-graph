bcbcf36f8e7f0e92642e3c4c31492c78723b2397
Nick Mancuso <nmancus1@dtcc.edu>
2018-11-22 09:00:53 -0500

    hw almost done

--- src/HW_7.java
+++ src/HW_7.java
@@ -1,51 +1,56 @@
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
 
 public class HW_7 {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
 
-        Vertex<Airport> lns = new Vertex(new Airport("LNS"));
-        Vertex<Airport> cth = new Vertex(new Airport("CTH"));
-        Vertex<Airport> ilg = new Vertex(new Airport("ILG"));
-        Vertex<Airport> phl = new Vertex(new Airport("PHL"));
 
-        lns.connect(phl,3);
-        lns.connect(cth,1);
-        lns.connect(ilg,2);
+        Vertex<Airport> atl = new Vertex(new Airport("ATL"));
+        Vertex<Airport> lax = new Vertex(new Airport("LAX"));
+        Vertex<Airport> ord = new Vertex(new Airport("ORD"));
+        Vertex<Airport> dfw = new Vertex(new Airport("DFW"));
+        Vertex<Airport> den = new Vertex(new Airport("DEN"));
+        Vertex<Airport> jfk = new Vertex(new Airport("JFK"));
+        Vertex<Airport> sfo = new Vertex(new Airport("SFO"));
+        Vertex<Airport> brw = new Vertex(new Airport("BRW"));
 
-        cth.connect(ilg,1);
-        cth.connect(lns,1);
-        cth.connect(phl,1);
 
-        ilg.connect(phl,1);
-        ilg.connect(cth,1);
-        ilg.connect(lns,2);
+        atl.connect(dfw,2);
+        atl.connect(jfk,2);
 
-        phl.connect(lns,3);
-        phl.connect(cth,1);
-        phl.connect(ilg,1);
+        lax.connect(dfw,3);
+        lax.connect(jfk,7);
+        lax.connect(sfo,1);
+
+        ord.connect(den, 2);
+        ord.connect(jfk,2);
+
+        dfw.connect(atl,2);
+        dfw.connect(lax,3);
+
+        den.connect(ord,2);
+        den.connect(sfo,3);
+
+        jfk.connect(atl,2);
+        jfk.connect(lax,7);
+        jfk.connect(ord,2);
+        jfk.connect(sfo,7);
+
+        sfo.connect(lax,1);
+        sfo.connect(den,3);
+        sfo.connect(jfk,7);
 
         HashMap<String, Vertex> map = new HashMap();
-        map.put("LNS", lns);
-        map.put("CTH", cth);
-        map.put("ILG", ilg);
-        map.put("PHL", phl);
+        map.put("ATL", atl);
+        map.put("LAX", lax);
+        map.put("ORD", ord);
+        map.put("DFW", )
 
 
-        for(Vertex v: map.values()) {
 
-            Iterator connectedFlights = v.getNeighborIterator();
-            Iterator connectFlightTime = v.getWeightIterator();
 
-            while (connectedFlights.hasNext() && connectFlightTime.hasNext()) {
-                System.out.print(v + " is connected to ");
-                System.out.print(connectedFlights.next());
-                System.out.println(" with a flight time of " + connectFlightTime.next() + " hours.");
 
-            }
-        }
 
-        }
+    }
+
 }
